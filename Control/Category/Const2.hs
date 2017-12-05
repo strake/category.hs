8a119621c1323d68e53777044c63f9ea72f91a88
M Farkas-Dyck <strake888@gmail.com>
2017-12-04 16:39:20 -0800

    const2

---
+++ Control/Category/Const2.hs
@@ -0,0 +1,14 @@
+module Control.Category.Const2 where
+
+import Algebra as A
+import Control.Category.Groupoid
+
+newtype Const2 a b c = Const2 a
+  deriving (Semigroup, Monoid, Group)
+
+instance Monoid a => Category (Const2 a) where
+    id = Const2 mempty
+    Const2 a . Const2 b = Const2 (a <> b)
+
+instance Group a => Groupoid (Const2 a) where
+    invert (Const2 a) = Const2 (A.invert a)
