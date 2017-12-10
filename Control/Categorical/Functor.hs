86ffbe93bdb55d5a531125e04561480ed4272460
M Farkas-Dyck <strake888@gmail.com>
2017-12-10 14:29:23 -0800

    rename `EndoFunctor` as `Endofunctor`

--- Control/Categorical/Functor.hs
+++ Control/Categorical/Functor.hs
@@ -15,7 +15,9 @@ import Data.Proxy
 class (Category s, Category t) => Functor (s :: α -> α -> *) (t :: β -> β -> *) (f :: α -> β) where
     map :: s a b -> t (f a) (f b)
 
-type EndoFunctor s f = Functor s s f
+{-# DEPRECATED EndoFunctor "Use Endofunctor" #-}
+type EndoFunctor s = Functor s s
+type Endofunctor s = Functor s s
 
 infixl 4 <$>
 (<$>) :: Functor s (->) f => s a b -> f a -> f b
